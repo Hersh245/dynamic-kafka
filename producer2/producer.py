5b28a9cc547f4120b3f9fd7f28134b52c5f73e4d
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2025-03-07 18:31:17 -0800

    producer 2 batch

--- producer2/producer.py
+++ producer2/producer.py
@@ -1,67 +1,57 @@
 import time
+import json
 from confluent_kafka import Producer
         
 class DynamicBatchProducer:
     def __init__(self, latency):
-        self.batch_size = 2
+        self.batch_size = 100000
         self.data = []
         self.target_latency = latency
-        self.current_latency = latency
+        self.delivery_latency = latency
+        self.max_latency = 0
         self.producer_conf = {
             "bootstrap.servers": "kafka:9092",
+            'statistics.interval.ms': 2000,
             "batch.size": self.batch_size,
-            "linger.ms": latency * self.batch_size,
+            "linger.ms": 10,
             }
         self.producer_conf["batch.size"] = self.batch_size
-        self.producer = Producer(self.producer_conf)
-        self.report = False
+        self.producer = Producer(self.producer_conf, stats_cb=self.stats_callback)
         
     def send_data(self, data, topic_name):
-        historical_cnt = 0
+        latencies = []
         for i in range(len(data)):
-            historical_cnt += 1
             self.producer.produce(topic_name, key=str(i), value=data[i], callback=self.delivery_report)
-            if historical_cnt >= self.batch_size:
-                historical_cnt = 0
-                self.producer.poll(1)
-                while(not self.report):
-                    time.sleep(0.5)
-                self.report = False
-                if self.current_latency > self.target_latency:
-                    self.producer.flush()
-                    self.producer_conf["batch.size"] = max(1,self.producer_conf["batch.size"] - 100)
-                    self.batch_size = self.producer_conf["batch.size"]
-                    self.producer = Producer(self.producer_conf)
-                    print("Batch size was changed")
-                else:
-                    print(self.current_latency)
+            self.producer.flush()
+            latencies.append(self.delivery_latency)
+            if sum(latencies)/len(latencies) > self.target_latency or latencies[-1] > self.target_latency * 1.5: 
+                self.producer_conf["batch.size"] = max(1,self.producer_conf["batch.size"] - 100)
+                self.batch_size = self.producer_conf["batch.size"]
+                self.producer = Producer(self.producer_conf, stats_cb=self.stats_callback)
+                print(f"Batch size was changed with latency {self.delivery_latency}")
         self.producer.flush()
+        print(f"last latency {self.delivery_latency}")
+        print(f"max latency is {max(latencies) * 1000000}")
                     
     def delivery_report(self, err, msg):
         if err is not None:
             print(f"Delivery failed for record {msg.key()}: {err}")
         else:
-            self.current_latency = msg.latency()
+            self.delivery_latency = msg.latency()
             self.report = True
-            print(
-                f"Producer2 successfully produced record to "
-                f"{msg.topic()} partition [{msg.partition()}] @ offset {msg.offset()} with latency {msg.latency()}"
-            )
-
+            # print(
+            #     f"Producer2 successfully produced record {msg.value()} to "
+            #     f"{msg.topic()} partition [{msg.partition()}] @ offset {msg.offset()} with latency {msg.latency()}"
+            # ) 
             
+    def stats_callback(self, stats_json):
+        stats = json.loads(stats_json)
+        if "brokers" in stats:
+            for broker_id, broker_data in stats["brokers"].items():
+                formatted_json = json.dumps(broker_data, indent=4)
+                print(formatted_json)
+                if "topics" in broker_data:
+                    for topic_name, topic_data in broker_data["topics"].items():
+                        print(f"Broker {broker_id}: topic {topic_name} has a batch sized average of {topic_data["batchsize"]["avg"]}")
+
         
-        
-# Producer2 config with a different batch size, linger, etc.
-
-
-# producer = Producer(producer_conf)
-topic_name = "mytopic"
-data = []
-for i in range(20):
-    value = f"Hello from Producer2 - {i}"
-    data.append(value)
-producer = DynamicBatchProducer(0.001)
-producer.send_data(data, topic_name)
-
-# producer.flush()
-# print("Producer2 finished sending messages.")
---
+++ producer2/test.py
@@ -0,0 +1,25 @@
+import random
+import string
+import time
+from producer import DynamicBatchProducer
+
+NUM_MSG = 10000
+MSG_SIZE = 1000
+
+def generate_payload(msg_size, num_msg):
+    payload = []
+    for i in range(num_msg):
+        msg = f"{i:06d} "
+        remaining_size = msg_size - len(msg.encode('utf-8'))
+        random_part = ''.join(random.choices(string.ascii_letters + string.digits, k=remaining_size))
+        payload.append((msg + random_part).encode('utf-8'))
+    return payload
+
+if __name__ == "__main__":
+    payload = generate_payload(MSG_SIZE, NUM_MSG)
+    producer = DynamicBatchProducer(0.2)
+    start_time = time.time()
+    producer.send_data(payload, "mytopic")
+    end_time = time.time()
+    print(f"end to end latency is {end_time - start_time}")
+    
