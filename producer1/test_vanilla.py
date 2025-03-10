4a7f497546047ab67ba517675a488490eecc81b7
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2025-03-10 02:06:42 -0700

    restore deleted files

---
+++ producer1/test_vanilla.py
@@ -0,0 +1,84 @@
+import random
+import json
+import string
+import time
+from confluent_kafka import Producer
+
+# Producer1 config with a certain batch.size, linger.ms, etc.
+producer_conf_list = [
+    {
+        "bootstrap.servers": "kafka:9092",
+        "batch.size": batch_size,  # Example batch size
+        "statistics.interval.ms": 1,
+        "linger.ms": 10,
+        # 'compression.type': 'lz4', etc. (optional)
+    }
+    for batch_size in range(1000, 20000, 1000)
+]
+
+latencies = []
+rtt = []
+
+def stats_callback(stats_json):
+    stats = json.loads(stats_json)
+    if "brokers" in stats:
+        for broker_id, broker_data in stats["brokers"].items():
+            if "rtt" in broker_data:
+                rtt.append(broker_data["rtt"]["avg"] * 1e-6)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Delivery failed for record {msg.key()}: {err}")
+    else:
+        latencies.append(msg.latency())
+
+
+NUM_MSG = 100000
+MSG_SIZE = 100
+
+
+def generate_payload(msg_size, num_msg):
+    payload = []
+    for i in range(num_msg):
+        msg = f"{i:06d} "
+        remaining_size = msg_size - len(msg.encode("utf-8"))
+        random_part = "".join(
+            random.choices(string.ascii_letters + string.digits, k=remaining_size)
+        )
+        payload.append((msg + random_part).encode("utf-8"))
+    return payload
+
+
+if __name__ == "__main__":
+    payload = generate_payload(MSG_SIZE, NUM_MSG)
+    time.sleep(10)
+    for producer_conf in producer_conf_list:
+        latencies = []
+        rtt = []
+        curr_sent_size = 0
+        producer = Producer(producer_conf, stats_cb=stats_callback)
+        start_time = time.time()
+        producer.produce("mytopic", key=str(0), value=payload[0], callback=delivery_report)
+        producer.flush()
+        batch_size = producer_conf['batch.size']
+        for i in range(1, NUM_MSG):
+            producer.produce("mytopic", key=str(i), value=payload[i], callback=delivery_report)
+            curr_sent_size += len(str(i).encode('utf-8')) + len(payload[i])
+            if curr_sent_size >= batch_size:
+                curr_sent_size = 0
+                producer.poll(0.1)
+            producer.poll(0)
+        # Wait for deliveries to complete
+        producer.flush()
+        end_time = time.time()
+        print(f"end to end latency is {end_time - start_time}")
+        with open(
+            f"vanilla_per_msg_latency_batchsize_{producer_conf['batch.size']}.txt", "a"
+        ) as file:
+            for l in latencies:
+                file.write(f"{l}\n")
+        with open(
+            f"vanilla_rtt_batchsize_{producer_conf['batch.size']}.txt", "a"
+        ) as file:
+            for l in rtt:
+                file.write(f"{l}\n")
---
+++ tests/copy_latency_file.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+CONTAINER_NAME="kafka-producer-1"
+
+# Destination directory on your local machine (Windows path)
+DEST_DIR="/Users/hanxu/OneDrive/Desktop/school/grad school/Freshman/CS 239/MuCache/dynamic-kakfa/tests"
+
+# Files to copy
+FILES=(
+  "vanilla_per_msg_latency_batchsize_1000.txt"
+  "vanilla_per_msg_latency_batchsize_2000.txt"
+  "vanilla_per_msg_latency_batchsize_3000.txt"
+  "vanilla_per_msg_latency_batchsize_4000.txt"
+  "vanilla_per_msg_latency_batchsize_5000.txt"
+  "vanilla_per_msg_latency_batchsize_6000.txt"
+  "vanilla_per_msg_latency_batchsize_7000.txt"
+  "vanilla_per_msg_latency_batchsize_8000.txt"
+  "vanilla_per_msg_latency_batchsize_9000.txt"
+  "vanilla_per_msg_latency_batchsize_10000.txt"
+  "vanilla_per_msg_latency_batchsize_11000.txt"
+  "vanilla_per_msg_latency_batchsize_12000.txt"
+  "vanilla_per_msg_latency_batchsize_13000.txt"
+  "vanilla_per_msg_latency_batchsize_14000.txt"
+  "vanilla_per_msg_latency_batchsize_15000.txt"
+  "vanilla_per_msg_latency_batchsize_16000.txt"
+  "vanilla_per_msg_latency_batchsize_17000.txt"
+  "vanilla_per_msg_latency_batchsize_18000.txt"
+  "vanilla_per_msg_latency_batchsize_19000.txt"
+  "vanilla_rtt_batchsize_1000.txt"
+  "vanilla_rtt_batchsize_2000.txt"
+  "vanilla_rtt_batchsize_3000.txt"
+  "vanilla_rtt_batchsize_4000.txt"
+  "vanilla_rtt_batchsize_5000.txt"
+  "vanilla_rtt_batchsize_6000.txt"
+  "vanilla_rtt_batchsize_7000.txt"
+  "vanilla_rtt_batchsize_8000.txt"
+  "vanilla_rtt_batchsize_9000.txt"
+  "vanilla_rtt_batchsize_10000.txt"
+  "vanilla_rtt_batchsize_11000.txt"
+  "vanilla_rtt_batchsize_12000.txt"
+  "vanilla_rtt_batchsize_13000.txt"
+  "vanilla_rtt_batchsize_14000.txt"
+  "vanilla_rtt_batchsize_15000.txt"
+  "vanilla_rtt_batchsize_16000.txt"
+  "vanilla_rtt_batchsize_17000.txt"
+  "vanilla_rtt_batchsize_18000.txt"
+  "vanilla_rtt_batchsize_19000.txt"
+)
+
+# FILES=(
+#   "dynamic_per_msg_latency.txt"
+#   "dynamic_rtt.txt"
+# )
+
+# FILES=(
+#   "pid_dynamic_per_msg_latency.txt"
+#   "pid_dynamic_rtt.txt"
+# )
+
+# Ensure the destination directory exists
+mkdir -p "$DEST_DIR"
+
+# Loop through and copy the files
+for FILE in "${FILES[@]}"; do
+  # Construct the full path within the container
+  CONTAINER_PATH="$FILE" # Assuming files are at the root of the container
+
+  # Construct the local destination path
+  LOCAL_PATH="$DEST_DIR/$FILE"
+
+  # Use docker cp to copy the file
+  docker cp "$CONTAINER_NAME:$CONTAINER_PATH" "$LOCAL_PATH"
+  echo "Copied: $FILE"
+done
+
+echo "Copy process completed."
